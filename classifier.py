3fb97c2ac53fe586cef42f7cdd77e8e740051aef
Claudiu <yatsukoyuno@gmail.com>
2020-04-07 17:26:50 +0300

    Added classifier logic

---
+++ classifier.py
@@ -0,0 +1,89 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+from scipy.special import expit
+
+
+def _sigmoid(x):
+    return expit(x)
+
+
+def _softmax(x, axis=-1):
+    x = x - np.amax(x, axis, keepdims=True)
+    e_x = np.exp(x)
+
+    return e_x / e_x.sum(axis, keepdims=True)
+
+
+def preprocess_input(image, net_h, net_w):
+    new_h, new_w, _ = image.shape
+
+    # determine the new size of the image
+    if (float(net_w)/new_w) < (float(net_h)/new_h):
+        new_h = (new_h * net_w)//new_w
+        new_w = net_w
+    else:
+        new_w = (new_w * net_h)//new_h
+        new_h = net_h
+
+    # resize the image to the new size
+    resized = cv2.resize(image[:, :, ::-1]/255., (new_w, new_h))
+
+    # embed the image into the standard letter box
+    new_image = np.ones((net_h, net_w, 3)) * 0.5
+    new_image[(net_h-new_h)//2:(net_h+new_h)//2,
+              (net_w-new_w)//2:(net_w+new_w)//2, :] = resized
+    new_image = np.expand_dims(new_image, 0)
+
+    return new_image
+
+
+def decode_netout(netout, anchors, obj_thresh, net_h, net_w):
+    grid_h, grid_w = netout.shape[:2]
+    nb_box = 3
+    netout = netout.reshape((grid_h, grid_w, nb_box, -1))
+    nb_class = netout.shape[-1] - 5
+
+    boxes = []
+    scores = []
+
+    netout[..., :2] = _sigmoid(netout[..., :2])
+    netout[..., 4] = _sigmoid(netout[..., 4])
+    netout[..., 5:] = netout[..., 4][..., np.newaxis] * \
+        _softmax(netout[..., 5:])
+    netout[..., 5:] *= netout[..., 5:] > obj_thresh
+
+    for i in range(grid_h*grid_w):
+        row = i // grid_w
+        col = i % grid_w
+        for b in range(nb_box):
+            scores.append(netout[row, col, b, 4])
+
+    return scores
+
+
+def get_yolo_score(model, images, net_h, net_w, anchors, obj_thresh, nms_thresh):
+    image_h, image_w, _ = images[0].shape
+    nb_images = len(images)
+    batch_input = np.zeros((nb_images, net_h, net_w, 3))
+
+    # preprocess the input
+    for i in range(nb_images):
+        batch_input[i] = preprocess_input(images[i], net_h, net_w)
+
+    # run the prediction
+    batch_output = model.predict_on_batch(batch_input)
+    batch_boxes = [None]*nb_images
+
+    for i in range(nb_images):
+        yolos = [batch_output[0][i], batch_output[1][i], batch_output[2][i]]
+        scores = []
+
+        # decode the output of the network
+        for j in range(len(yolos)):
+            # config['model']['anchors']
+            yolo_anchors = anchors[(2-j)*6:(3-j)*6]
+            scores += decode_netout(yolos[j],
+                                    yolo_anchors, obj_thresh, net_h, net_w)
+
+    return max(scores)
